90da026b9d7ee46aa82ebdf4e3a7fc33b373dfdf
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-14 15:05:13 +0400

    save customer in bd and show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,22 @@
 class CustomersController < ApplicationController
   
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:customer].inspect
+    @customer = Customer.new(customer_params)
+
+    @customer.save
+    redirect_to @customer
   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:name, :phone, :text)
+    end
 
 end
