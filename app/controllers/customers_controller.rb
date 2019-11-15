9d47eb80faf95ec4efd23cd9044fd54dbf66dd4b
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-15 09:12:38 +0400

    add update

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,6 +12,10 @@ class CustomersController < ApplicationController
     @customer = Customer.new
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
   def create
     @customer = Customer.new(customer_params)
 
@@ -22,9 +26,18 @@ class CustomersController < ApplicationController
     end
   end
 
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      render 'edit'
+    end
+  end
+
   private
     def customer_params
-      params.require(:customer).permit(:name, :phone, :text)
+      params.require(:customer).permit(:name, :phone, :text, :black_list)
     end
 
 end
