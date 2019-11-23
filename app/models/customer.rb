2dfec38ce72774736ce21a7bc93a390184346ec4
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-11-23 12:50:45 +0400

    test for Heroku

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,8 @@ class Customer < ApplicationRecord
 
   def self.search(search, id)
     if search
-      @customers = Customer.where('id LIKE ?', "%#{search}%").or(Customer.where('phone LIKE ?', "%#{search}%")).or(Customer.where('name LIKE ?', "%#{search}%"))
+      @customers = Customer.where('id LIKE ?', "%#{search}%")
+      # .or(Customer.where('phone LIKE ?', "%#{search}%")).or(Customer.where('name LIKE ?', "%#{search}%"))
     else
      @customers = Customer.all
     end
