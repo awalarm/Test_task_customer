d2fac15bb7ba6682f90b025f661f1f094659bf7f
Pasha Shalunov <shalunov.pasha@gmail.com>
2019-12-03 16:33:16 +0400

    install gem devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'customers/blacklist'
 
   put 'customers/blacklistphone'
 
   resources :customers
 
-  root 'customers#index'
+  root to: "customers#index"
+
+
+  # root 'customers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
